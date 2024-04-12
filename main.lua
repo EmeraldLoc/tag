17a66ed11fbef5efe5295a3969aed36ebac09eae
Emerald Lockdown <calebelm@icloud.com>
2024-04-11 21:11:26 -0500

    Whoopsies (pt.4)

--- main.lua
+++ main.lua
@@ -894,17 +894,23 @@ local function update()
         for i = MIN_GAMEMODE, MAX_GAMEMODE do
             if load_int("stats_" .. i .. "_playTime") ~= nil then
                 stats[i].playTime = load_int("stats_" .. i .. "_playTime")
-            elseif load_int("stats_" .. i .. "_runnerVictories") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_runnerVictories") ~= nil then
                 stats[i].runnerVictories = load_int("stats_" .. i .. "_runnerVictories")
-            elseif load_int("stats_" .. i .. "_taggerVictories") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_taggerVictories") ~= nil then
                 stats[i].taggerVictories = load_int("stats_" .. i .. "_taggerVictories")
-            elseif load_int("stats_" .. i .. "_victories") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_victories") ~= nil then
                 stats[i].victories = load_int("stats_" .. i .. "_victories")
-            elseif load_int("stats_" .. i .. "_totalTimeAsRunner") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_totalTimeAsRunner") ~= nil then
                 stats[i].totalTimeAsRunner = load_int("stats_" .. i .. "_totalTimeAsRunner")
-            elseif load_int("stats_" .. i .. "_totalTags") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_totalTags") ~= nil then
                 stats[i].totalTags = load_int("stats_" .. i .. "_totalTags")
-            elseif load_int("stats_" .. i .. "_totalTimeAsSardine") ~= nil then
+            end
+            if load_int("stats_" .. i .. "_totalTimeAsSardine") ~= nil then
                 stats[i].totalTimeAsSardine = load_int("stats_" .. i .. "_totalTimeAsSardine")
             end
         end
