58f203589a312c460ea2d674d22fc57a66f6e4dd
Emerald Lockdown <calebelm@icloud.com>
2024-04-12 17:05:21 -0500

    Fix lives problem

--- deathmatch.lua
+++ deathmatch.lua
@@ -95,7 +95,7 @@ local function hud_render()
 end
 
 ---@param m MarioState
-local function on_death(m)
+local function on_warp(m)
 
     if gGlobalSyncTable.gamemode ~= DEATHMATCH then return end
     if gGlobalSyncTable.roundState ~= ROUND_ACTIVE then return end
@@ -176,5 +176,5 @@ hook_event(HOOK_MARIO_UPDATE, mario_update)
 hook_event(HOOK_ON_HUD_RENDER, hud_render)
 hook_event(HOOK_ALLOW_PVP_ATTACK, allow_pvp)
 hook_event(HOOK_ON_PVP_ATTACK, on_pvp)
-hook_event(HOOK_ON_DEATH, on_death)
+hook_event(HOOK_ON_WARP, on_warp) -- treat like on death
 hook_event(HOOK_ALLOW_INTERACT, allow_interact)
