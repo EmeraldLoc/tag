894525817542ebc05c743e8dc360a9ad48ea865a
Emerald Lockdown <calebelm@icloud.com>
2024-05-21 22:29:05 -0500

    Allow spectators to stand on hard surfaces

--- hard_surface_modifier.lua
+++ hard_surface_modifier.lua
@@ -1,8 +1,9 @@
 
 ---@param m MarioState
 local function mario_update(m)
-
-    if gGlobalSyncTable.modifier ~= MODIFIER_HARD_SURFACE then return end
+    if  gGlobalSyncTable.modifier ~= MODIFIER_HARD_SURFACE
+    and gPlayerSyncTable[m.playerIndex].state ~= SPECTATOR
+    and gPlayerSyncTable[m.playerIndex].state ~= WILDCARD_ROLE then return end
 
     if m.floor.type ~= SURFACE_DEATH_PLANE
     and (m.floor.type ~= SURFACE_BURNING or not gGlobalSyncTable.hazardSurfaces) then
