6fca345b4c5345e66e22198521e9e94a3e2524b8
Emerald Lockdown <calebelm@icloud.com>
2024-05-26 17:37:58 -0500

    Fix lava b oost in low gravity

--- low_gravity_modifier.lua
+++ low_gravity_modifier.lua
@@ -4,11 +4,12 @@ local function before_phys_step(m)
     if gGlobalSyncTable.modifier ~= MODIFIER_LOW_GRAVITY then return end
 
     if m.vel.y > 0 then
-        -- check if action is getting blown or twirling because these actions have a high velocity acceleration
+        -- check if action is a certain action because some actions have a high velocity acceleration
         if  m.action ~= ACT_TWIRLING
         and m.action ~= ACT_GETTING_BLOWN
         and m.action ~= ACT_FLYING_TRIPLE_JUMP
         and m.action ~= ACT_SHOT_FROM_CANNON
+        and m.action ~= ACT_LAVA_BOOST
         and not interactedWithSpring  then
             m.vel.y = m.vel.y * 1.05
         end
